7b0ee2f908c98e634af8b455c2923c101067bf96
Jason Antwi-Appah <hey@jasonaa.me>
2020-11-26 06:09:32 +0000

    feat\!: added many new things - enabled saving and loading text to browser's localStorage, added copy to clipboard button and light/dark mode switcher. also made everything slightly less ugly

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,74 @@
 // lol this whole thing probably could be done in react but ssr go brrr
 
+// feat!: a lot of things: enabled saving and loading text to browser's localStorage, added copy to clipboard button and light/dark mode switcher. also made everything slightly less ugly
+
 import Head from "next/head";
 import { useState } from "react";
-import { Page, Textarea, Text, Link, Grid, Spacer } from "@geist-ui/react";
+import {
+  Page,
+  Textarea,
+  Text,
+  Link,
+  Grid,
+  Spacer,
+  Button,
+  useToasts,
+  useClipboard,
+  Toggle,
+  Col,
+  User,
+  Note,
+  useTheme,
+  Row,
+} from "@geist-ui/react";
+import { Copy, Github } from "@geist-ui/react-icons";
 
-export default function Home() {
-  const [text, setText] = useState({
-    "chars": 0,
-    "words": 0,
-    "sentences": 0
-  });
+const center = { textAlign: "center" };
+
+export default function Home(props) {
+  const [, setToast] = useToasts();
+  const theme = useTheme();
+  const { copy } = useClipboard();
+  const defaultStats = getStatsFromStorage() || {
+    chars: 0,
+    words: 0,
+    sentences: 0,
+  };
+
+  const [text, setText] = useState(getTextFromStorage());
+  const [textStats, setStats] = useState(defaultStats);
   function onChange(e) {
     const temp = e.target.value;
-    setText({
+    setText(temp);
+    localStorage.setItem("text", temp);
+    setStats({
       chars: temp.length,
       // i love/hate regex but its kinda cool when it works.
       // s/o https://regexr.com/
-      words: (temp.split(/(!+|\?+|\.+| +)/).length-1)/2,
-      sentences: (temp.split(/(!+|\?+|\.+)/).length - 1)/2,
+      words: (temp.split(/(!+|\?+|\.+| +)/).length - 1) / 2,
+      sentences: (temp.split(/(!+|\?+|\.+)/).length - 1) / 2,
     });
+    localStorage.setItem("stats", JSON.stringify(textStats));
   }
+
+  function getTextFromStorage() {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("text");
+    } else {
+      return "";
+    }
+  }
+
+  function getStatsFromStorage() {
+    if (typeof window !== "undefined") {
+      return JSON.parse(localStorage.getItem("stats"));
+    } else {
+      return "";
+    }
+  }
+
   return (
-    <Page>
+    <Page dotBackdrop>
       <Head>
         <title>Word Counter</title>
         <link rel="icon" href="/favicon.ico" />
@@ -31,44 +78,90 @@ export default function Home() {
           rel="stylesheet"
         ></link>
       </Head>
-      <Page.Header>
-        <Text h1>Word Count.</Text>
-      </Page.Header>
-      <Page.Content>
-        <Textarea
-          onChange={onChange}
-          width="100%"
-          placeholder="Enter your text here."
-        />
-        <Grid.Container gap={2} justify="center">
-          <Grid xs>
-          <Info noun="character" no={text.chars}></Info>
-          </Grid>
-          <Grid xs>
-          <Info noun="word" no={text.words}></Info>
-          </Grid>
-          <Grid xs>
-            <Info noun="sentence" no={text.sentences}></Info>
-          </Grid>
-        </Grid.Container>
-      </Page.Content>
-      <Page.Footer>
-        <Text>
-          Built by <Link href="https://jasonaa.me/g">@jasonappah</Link>
-        </Text>
-        <Spacer></Spacer>
-      </Page.Footer>
+      <Col align="middle">
+        <Page.Header>
+          <Text style={center} h1>
+            Word Count.
+          </Text>
+          <Text p style={center}>
+            Enter your text below and see your current word count! Don't worry
+            about saving - text will auto-save to your browser's storage after
+            every edit.
+          </Text>
+        </Page.Header>
+        <Page.Content>
+          <Textarea
+            status="success"
+            id="textarea"
+            onChange={onChange}
+            value={text}
+            width="100%"
+            placeholder="Enter your text here."
+          />
+          <Grid.Container alignItems="center" justify="space-evenly">
+            <Grid xs>
+              <Info noun="character" no={textStats.chars}></Info>
+            </Grid>
+            <Grid xs>
+              <Info noun="word" no={textStats.words}></Info>
+            </Grid>
+            <Grid xs>
+              <Info noun="sentence" no={textStats.sentences}></Info>
+            </Grid>
+          </Grid.Container>
+
+          <Grid.Container alignItems="center" justify="space-evenly">
+            <Button
+              icon={<Copy />}
+              auto
+              onClick={() => {
+                copy(text);
+                setToast({ text: "Text copied.", type: "success" });
+              }}
+            >
+              Copy to Clipboard
+            </Button>
+            <Note
+              small
+              type="secondary"
+              label={"Theme"}
+              style={{ marginTop: "1em", width: "10em" }}
+            >
+              <Spacer y={0.5} />
+              <Toggle
+                name="Dark Mode"
+                onChange={props.themeToggle}
+                initialChecked={props.currentTheme == "light" ? false : true}
+              />
+            </Note>
+          </Grid.Container>
+        </Page.Content>
+
+        <Row justify="center">
+          <User
+            src="https://gravatar.com/avatar/d35776f3bec9c6459903f6a3204b63e4"
+            name="Built by"
+          >
+            <User.Link href="https://github.com/jasonappah">
+              @jasonappah
+            </User.Link>
+          </User>
+          <Link href="https://github.com/jasonappah/wordcounter">
+            <Button iconRight={<Github />} auto />
+          </Link>
+        </Row>
+      </Col>
     </Page>
   );
 }
 
 function Info(props) {
   const noun = props.noun || "";
-  const no = props.no || 0
-  console.log(props.no)
+  const no = props.no || 0;
   return (
-    <Text>
-      {no} {noun}{no == 1 ? "" : "s"}.
+    <Text p b style={center}>
+      {no} {noun}
+      {no == 1 ? "" : "s"}.
     </Text>
   );
 }

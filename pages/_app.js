87519c2c84c8af41823a7e7e839cdf52a0f55241
Jason Antwi-Appah <hey@jasonaa.me>
2020-11-26 05:53:46 +0000

    feat: started work on theme switcher

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { useState } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [themeType, setThemeType] = useState("light");
+  const switchThemes = () => {
+    setThemeType((lastThemeType) =>
+      lastThemeType === "dark" ? "light" : "dark"
+    );
+  };
+  return (
+    <GeistProvider theme={{ type: themeType }}>
+      <CssBaseline />
+      <Component
+        {...pageProps}
+        themeToggle={switchThemes}
+        currentTheme={themeType}
+      />
+    </GeistProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

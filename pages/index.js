9b1511d425385cb3420c4daa13a653d7a8bad5fa
Steven Conaway <steven.conaway@icloud.com>
2020-12-06 13:33:17 -0800

    Save dark mode choice in localStorage
    Seems to work, although it throws this warning if started into dark mode:
    
    ```
    Prop `className` did not match. Server: "jsx-85500106 " Client: "jsx-822878187 "
    ```

--- pages/index.js
+++ pages/index.js
@@ -1,192 +1,208 @@
 // lol this whole thing probably could be done in react but ssr go brrr
 
-import Head from "next/head";
-import { useState } from "react";
+import Head from 'next/head';
+import {useState} from 'react';
 import {
-  Page,
-  Textarea,
-  Text,
-  Link,
-  Grid,
-  Spacer,
-  Button,
-  useToasts,
-  useClipboard,
-  Toggle,
-  Col,
-  User,
-  Note,
-  useTheme,
-  Row,
-} from "@geist-ui/react";
-import { Copy, Github } from "@geist-ui/react-icons";
+    Page,
+    Textarea,
+    Text,
+    Link,
+    Grid,
+    Spacer,
+    Button,
+    useToasts,
+    useClipboard,
+    Toggle,
+    Col,
+    User,
+    Note,
+    useTheme,
+    Row,
+} from '@geist-ui/react';
+import {Copy, Github} from '@geist-ui/react-icons';
 
 const metaImg =
-  "https://f000.backblazeb2.com/file/jasonaa-static/img/wordcounter.png";
-const center = { textAlign: "center" };
+    'https://f000.backblazeb2.com/file/jasonaa-static/img/wordcounter.png';
+const center = {textAlign: 'center'};
+
+function getTheme() {
+    if (typeof Storage !== 'undefined') {
+        // can use window.localStorage, so use the set value if it exists
+        if (localStorage.getItem('preferredTheme') === 'dark') return 'dark';
+    }
+    return 'light';
+}
 
 export default function Home(props) {
-  const [, setToast] = useToasts();
-  const theme = useTheme();
-  const { copy } = useClipboard();
-  const defaultStats = getStatsFromStorage() || {
-    chars: 0,
-    words: 0,
-    sentences: 0,
-  };
+    const [, setToast] = useToasts();
+    const theme = useTheme();
+    const {copy} = useClipboard();
+    const defaultStats = getStatsFromStorage() || {
+        chars: 0,
+        words: 0,
+        sentences: 0,
+    };
 
-  const [text, setText] = useState(getTextFromStorage() || "");
-  const [textStats, setStats] = useState(defaultStats);
-  function onChange(e) {
-    const temp = e.target.value;
-    setText(temp);
-    localStorage.setItem("text", temp);
-    const tmp = temp.split(/([A-z])+/) || [];
-    setStats({
-      chars: temp.length,
-      // i love/hate regex but its kinda cool when it works.
-      // s/o https://regexr.com/
-      words: (tmp.length - 1) / 2,
-      sentences: (temp.split(/(!+|\?+|\.+)/).length - 1) / 2,
-    });
-    localStorage.setItem("stats", JSON.stringify(textStats));
-  }
-
-  function getTextFromStorage() {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("text");
-    } else {
-      return "";
+    const [text, setText] = useState(getTextFromStorage() || '');
+    const [textStats, setStats] = useState(defaultStats);
+    function onChange(e) {
+        const temp = e.target.value;
+        setText(temp);
+        localStorage.setItem('text', temp);
+        const tmp = temp.split(/([A-z])+/) || [];
+        setStats({
+            chars: temp.length,
+            // i love/hate regex but its kinda cool when it works.
+            // s/o https://regexr.com/
+            words: (tmp.length - 1) / 2,
+            sentences: (temp.split(/(!+|\?+|\.+)/).length - 1) / 2,
+        });
+        localStorage.setItem('stats', JSON.stringify(textStats));
     }
-  }
 
-  function getStatsFromStorage() {
-    if (typeof window !== "undefined") {
-      return JSON.parse(localStorage.getItem("stats"));
-    } else {
-      return "";
+    function getTextFromStorage() {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem('text');
+        } else {
+            return '';
+        }
     }
-  }
 
-  return (
-    <Page dotBackdrop>
-      <Head>
-        <title>Word Counter</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        ></link>
-        <meta name="title" content="Word Counter" />
-        <meta
-          name="description"
-          content="Keep track of your document's word count!"
-        />
+    function getStatsFromStorage() {
+        if (typeof window !== 'undefined') {
+            return JSON.parse(localStorage.getItem('stats'));
+        } else {
+            return '';
+        }
+    }
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://wc.jasonaa.me/" />
-        <meta property="og:title" content="Word Counter" />
-        <meta
-          property="og:description"
-          content="Keep track of your document's word count!"
-        />
-        <meta property="og:image" content={metaImg} />
+    return (
+        <Page dotBackdrop={getTheme === 'light'}>
+            <Head>
+                <title>Word Counter</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+                <meta name="title" content="Word Counter" />
+                <meta
+                    name="description"
+                    content="Keep track of your document's word count!"
+                />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://wc.jasonaa.me/" />
-        <meta property="twitter:title" content="Word Counter" />
-        <meta
-          property="twitter:description"
-          content="Keep track of your document's word count!"
-        />
-        <meta property="twitter:image" content={metaImg} />
-      </Head>
-      <Col align="middle">
-        <Page.Header>
-          <Text style={center} h1>
-            Word Count.
-          </Text>
-          <Text p style={center}>
-            Enter your text below and see your current word count! Don't worry
-            about saving - text will auto-save to your browser's storage after
-            every edit.
-          </Text>
-        </Page.Header>
-        <Page.Content style={{padding: "calc(1pt * 2.5) 0"}}>
-          <Textarea
-            status="success"
-            id="textarea"
-            onChange={onChange}
-            value={text}
-            width="100%"
-            minHeight="30em"
-            placeholder="Enter your text here."
-          />
-          <Grid.Container alignItems="center" justify="space-evenly">
-            <Grid xs>
-              <Info noun="character" no={textStats.chars}></Info>
-            </Grid>
-            <Grid xs>
-              <Info noun="word" no={textStats.words}></Info>
-            </Grid>
-            <Grid xs>
-              <Info noun="sentence" no={textStats.sentences}></Info>
-            </Grid>
-          </Grid.Container>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://wc.jasonaa.me/" />
+                <meta property="og:title" content="Word Counter" />
+                <meta
+                    property="og:description"
+                    content="Keep track of your document's word count!"
+                />
+                <meta property="og:image" content={metaImg} />
 
-          <Grid.Container alignItems="center" justify="space-evenly">
-            <Button
-              icon={<Copy />}
-              auto
-              onClick={() => {
-                copy(text);
-                setToast({ text: "Text copied.", type: "success" });
-              }}
-            >
-              Copy to Clipboard
-            </Button>
-            <Note
-              small
-              type="secondary"
-              label={"Theme"}
-              style={{ marginTop: "1em", width: "10em" }}
-            >
-              <Spacer y={0.5} />
-              <Toggle
-                name="Dark Mode"
-                onChange={props.themeToggle}
-                initialChecked={props.currentTheme == "light" ? false : true}
-              />
-            </Note>
-          </Grid.Container>
-        </Page.Content>
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://wc.jasonaa.me/" />
+                <meta property="twitter:title" content="Word Counter" />
+                <meta
+                    property="twitter:description"
+                    content="Keep track of your document's word count!"
+                />
+                <meta property="twitter:image" content={metaImg} />
+            </Head>
+            <Col align="middle">
+                <Page.Header>
+                    <Text style={center} h1>
+                        Word Count.
+                    </Text>
+                    <Text p style={center}>
+                        Enter your text below and see your current word count!
+                        Don't worry about saving - text will auto-save to your
+                        browser's storage after every edit.
+                    </Text>
+                </Page.Header>
+                <Page.Content style={{padding: 'calc(1pt * 2.5) 0'}}>
+                    <Textarea
+                        status="success"
+                        id="textarea"
+                        onChange={onChange}
+                        value={text}
+                        width="100%"
+                        minHeight="30em"
+                        placeholder="Enter your text here."
+                    />
+                    <Grid.Container alignItems="center" justify="space-evenly">
+                        <Grid xs>
+                            <Info noun="character" no={textStats.chars}></Info>
+                        </Grid>
+                        <Grid xs>
+                            <Info noun="word" no={textStats.words}></Info>
+                        </Grid>
+                        <Grid xs>
+                            <Info
+                                noun="sentence"
+                                no={textStats.sentences}
+                            ></Info>
+                        </Grid>
+                    </Grid.Container>
 
-        <Row justify="center" style={{ margin: "1em" }}>
-          <User
-            src="https://gravatar.com/avatar/d35776f3bec9c6459903f6a3204b63e4"
-            name="Built by"
-          >
-            <User.Link href="https://github.com/jasonappah">
-              @jasonappah
-            </User.Link>
-          </User>
-          <Link href="https://github.com/jasonappah/wordcounter">
-            <Button iconRight={<Github />} auto />
-          </Link>
-        </Row>
-      </Col>
-    </Page>
-  );
+                    <Grid.Container alignItems="center" justify="space-evenly">
+                        <Button
+                            icon={<Copy />}
+                            auto
+                            onClick={() => {
+                                copy(text);
+                                setToast({
+                                    text: 'Text copied.',
+                                    type: 'success',
+                                });
+                            }}
+                        >
+                            Copy to Clipboard
+                        </Button>
+                        <Note
+                            small
+                            type="secondary"
+                            label={'Theme'}
+                            style={{marginTop: '1em', width: '10em'}}
+                        >
+                            <Spacer y={0.5} />
+                            <Toggle
+                                name="Dark Mode"
+                                onChange={props.themeToggle}
+                                initialChecked={
+                                    props.currentTheme == 'light' ? false : true
+                                }
+                            />
+                        </Note>
+                    </Grid.Container>
+                </Page.Content>
+
+                <Row justify="center" style={{margin: '1em'}}>
+                    <User
+                        src="https://gravatar.com/avatar/d35776f3bec9c6459903f6a3204b63e4"
+                        name="Built by"
+                    >
+                        <User.Link href="https://github.com/jasonappah">
+                            @jasonappah
+                        </User.Link>
+                    </User>
+                    <Link href="https://github.com/jasonappah/wordcounter">
+                        <Button iconRight={<Github />} auto />
+                    </Link>
+                </Row>
+            </Col>
+        </Page>
+    );
 }
 
 function Info(props) {
-  const noun = props.noun || "";
-  const no = props.no || 0;
-  return (
-    <Text p b style={center}>
-      {no} {noun}
-      {no == 1 ? "" : "s"}.
-    </Text>
-  );
+    const noun = props.noun || '';
+    const no = props.no || 0;
+    return (
+        <Text p b style={center}>
+            {no} {noun}
+            {no == 1 ? '' : 's'}.
+        </Text>
+    );
 }
